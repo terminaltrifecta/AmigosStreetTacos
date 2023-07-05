90db018848fd0362df093c712d4821a1aee9d796
speedybeaver <gmandwee@gmail.com>
2023-07-05 18:55:09 -0400

    styled menu accordion

--- src/routes/menu.tsx
+++ src/routes/menu.tsx
@@ -5,23 +5,27 @@ import ImageOverlay from "../components/ImageOverlay/ImageOverlay";
 export default function Menu() {
   return (
     <React.StrictMode>
-      <ImageOverlay />
-      <AccordionMenu
-        acchdr1="Breakfast - $5"
-        acchdr2="Bowls - $10"
-        acchdr3="Quesadillas - $9"
-        acchdr4="Burritos - $10"
-        acchdr5="Tacos - $2.85"
-        acchdr6="Gourmet Tacos - $3.50"
-        acchdr7="Amigos Specials"
-        acctxt1="poop"
-        acctxt2="poop"
-        acctxt3="poop"
-        acctxt4="poop"
-        acctxt5="poop"
-        acctxt6="poop"
-        acctxt7="poop"
-      />
+      <div className="d-grid">
+        <ImageOverlay />
+        <div className="p-4">
+          <AccordionMenu
+            acchdr1="Breakfast - $5"
+            acchdr2="Bowls - $10"
+            acchdr3="Quesadillas - $9"
+            acchdr4="Burritos - $10"
+            acchdr5="Tacos - $2.85"
+            acchdr6="Gourmet Tacos - $3.50"
+            acchdr7="Amigos Specials"
+            acctxt1="poop"
+            acctxt2="poop"
+            acctxt3="poop"
+            acctxt4="poop"
+            acctxt5="poop"
+            acctxt6="poop"
+            acctxt7="poop"
+          />
+        </div>
+      </div>
     </React.StrictMode>
   );
 }
