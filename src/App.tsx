d399cf467f198a4eaac8eb27f98688fcb32ee53f
zirppo <zirppo@protonmail.com>
2023-07-04 22:32:53 -0400

    added buttons, css

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Order from "./routes/order.tsx";
 import Menu from "./routes/menu.tsx";
 import Navbar from "./components/Navbar/Navbar.tsx";
 import Footer from "./components/Footer/Footer.tsx";
+import './App.css';
 
 function App() {
   return (
