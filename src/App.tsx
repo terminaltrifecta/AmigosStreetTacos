ccf06d5fb5d792b1a65709db8e88eeef938db81a
casey <zirppo@protonmail.com>
2023-07-09 15:00:55 -0400

    sorry for committing so much

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import Root from "./routes/root.tsx";
 import About from "./routes/about.tsx";
 import Locations from "./routes/locations.tsx";
-import Order from "./routes/order.tsx";
 import Menu from "./routes/menu.tsx";
 import Navbar from "./components/Navbar/Navbar.tsx";
 import Footer from "./components/Footer/Footer.tsx";
