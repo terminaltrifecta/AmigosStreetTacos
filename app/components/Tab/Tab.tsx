11d6618394d568075680cfa414ccd901ccabc42f
Gio Mandwee <gmandwee@gmail.com>
2024-08-17 22:21:43 -0400

    Started next js port from a blank template, and moved the "root.tsx" route

--- app/components/Tab/Tab.tsx
+++ app/components/Tab/Tab.tsx
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "./Tab.css";
+import Image from "next/image";
 
 function Tab({ text, img, textRight }: any) {
   return (
@@ -8,10 +9,12 @@ function Tab({ text, img, textRight }: any) {
         <div className="col d-flex align-items-center justify-content-center tabcol">
           {textRight === "true" ? (
             <div>
-              <img
+              <Image
                 src={img}
                 alt=""
                 className="img-fluid rounded-4 border border-5"
+                width={1920}
+                height={1080}
               />
             </div>
           ) : (
@@ -23,10 +26,12 @@ function Tab({ text, img, textRight }: any) {
             <p className="tabtext">{text}</p>
           ) : (
             <div>
-              <img
+              <Image
                 src={img}
                 alt=""
                 className="img-fluid rounded-4 border border-5"
+                width={1920}
+                height={1080}
               />
             </div>
           )}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
-import CardTab from "../components/CardTab/CardTab.tsx";
-import Slideshow from "../components/Slideshow/Slideshow.tsx";
-import Tab from "../components/Tab/Tab.tsx";
+'use client'
 
-export default function Root() {
+import Image from "next/image";
+import styles from "./page.module.css";
+import Slideshow from "./components/Slideshow/Slideshow";
+import Tab from "./components/Tab/Tab";
+import CardTab from "./components/CardTab/CardTab";
+
+export default function Home() {
   return (
     <>
       <div className="d-grid gap-3">
@@ -10,40 +14,40 @@ export default function Root() {
         <Tab
           text="Carne Asada Fries. They're f#cking delicious."
           textRight="true"
-          img="\static\assets\home\fries.jpg"
+          img="/static/assets/home/fries.jpg"
         />
         <Tab
           text="Enjoy real Mexican from the comfort of your home with online delivery services. You can find us anywhere!"
           textRight="false"
-          img="\static\assets\home\delivery.jpg"
+          img="/static/assets/home/delivery.jpg"
         />
         <CardTab
-          img1="\static\assets\slideshow\img1.png"
+          img1="/static/assets/slideshow/img1.png"
           hdr1="Chicken Tacos"
-          img2="\static\assets\home\carnetacos.jpg"
+          img2="/static/assets/home/carnetacos.jpg"
           hdr2="Carne Asada Tacos"
-          img3="\static\assets\home\barriatacos.jpg"
+          img3="/static/assets/home/barriatacos.jpg"
           hdr3="Birria Tacos"
         />
         <Tab
           text="Love what you see? Take everything home with our quick carry-out."
           textRight="false"
-          img="\static\assets\home\carryout.jpg"
+          img="/static/assets/home/carryout.jpg"
         />
         <Tab
           text="We have amazing deals as well! Get 5 tacos for ten dollars anyday, or a special $2.00 per street taco on Taco Tuesdays!"
           textRight="true"
-          img="\static\assets\home\tacosdeal.jpeg"
+          img="/static/assets/home/tacosdeal.jpeg"
         />
         <Tab
           text="Like what you see? Come and try our birria tacos today!"
           textRight="false"
-          img="\static\assets\home\birria_tacos_AdobeExpress.gif"
+          img="/static/assets/home/birria_tacos_AdobeExpress.gif"
         />
         <Tab
           text="Be sure to try our lunch combo! It comes 2 tacos, rice and beans, a can of pop, and chips and salsa! All for $12!"
           textRight="true"
-          img="\static\assets\home\riceandbeans.jpg"
+          img="/static/assets/home/riceandbeans.jpg"
         />
       </div>
     </>
