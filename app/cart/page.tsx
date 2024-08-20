3acfcbe36d214f2eea975cb4c6d8e93315d853a2
speedybeaver <gmandwee@gmail.com>
2024-08-20 15:33:29 -0400

    tailwind shit

---
+++ app/cart/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function cart() {
+  return (
+    <div>
+      <div className="p-4 space-y-1.5 w-100">
+        <div className="border-4 border-black-400">
+            Hi
+        </div>
+        <div className="">
+            Byebye
+        </div>
+      </div>
+      <div className="p-4 border-4">
+        Checkout
+      </div>
+    </div>
+  );
+}
+
+export default cart;
